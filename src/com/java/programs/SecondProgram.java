08d3d9cf74208b102b45871189deb4243523fd5f
Abhi7642 <abhi7642@yahoo.in>
2022-02-24 12:22:58 +0530

    Added new check

--- src/com/java/programs/SecondProgram.java
+++ src/com/java/programs/SecondProgram.java
@@ -4,7 +4,7 @@ public class SecondProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Second Program");
+		System.out.println("Second Program in Java");
 
 	}
 
